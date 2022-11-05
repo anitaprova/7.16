33215e7d833f463314583422cd7f0e1be4afccf2
anitaprova <104943181+anitaprova@users.noreply.github.com>
2022-11-05 19:19:10 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+struct Point
+{
+	double x;
+	double y;
+};
+
+double distance(Point a, Point b) {
+	return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
+}
+
+
+int main() {	
+	Point p1;
+	Point p2;
+	double a, b, c, d = 0.0;
+	
+	cin >> a >> b >> c >> d;
+	p1.x = a;
+	p1.y = b;
+	p2.x = c;
+	p2.y = d;
+	
+	cout << distance(p1, p2) << endl;
+	
+	return 0;
+}
